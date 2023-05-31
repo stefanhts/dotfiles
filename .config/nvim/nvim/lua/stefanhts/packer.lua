773c44dfad040c9493737e1d9c23cbde2aadda7b
Stefan Heller <stefan.heller@okta.com>
2023-05-31 18:35:44 -0400

    feat: work config

---
+++ .config/nvim/nvim/lua/stefanhts/packer.lua
@@ -0,0 +1,65 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+require("mason").setup()
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+
+return require('packer').startup(function(use)
+
+	use {
+	  'VonHeikemen/lsp-zero.nvim', branch = 'v2.x', requires =
+	  {
+	    -- LSP Support {'neovim/nvim-lspconfig'},             --
+	    Required {                                      --
+	    Optional
+	      'williamboman/mason.nvim', run = function()
+		pcall(vim.cmd, 'MasonUpdate')
+	      end,
+	    }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	    -- Autocompletion {'hrsh7th/nvim-cmp'},     -- Required
+	    {'hrsh7th/cmp-nvim-lsp'}, -- Required {'L3MON4D3/LuaSnip'},
+	    -- Required
+	  }
+	}
+      -- Packer can manage itself use 'wbthomason/packer.nvim'
+
+      use 'f-person/git-blame.nvim'
+
+      use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0', -- or
+	  , branch = '0.1.x', requires = { {'nvim-lua/plenary.nvim'}
+	  }
+      }
+	use { 'nvim-lualine/lualine.nvim', requires = {
+	'nvim-tree/nvim-web-devicons', opt = true } }
+
+      use({
+	  'rose-pine/neovim', as = 'rose-pine', config = function()
+	      vim.cmd('colorscheme rose-pine')
+	  end
+      })
+
+      use({
+	  "folke/trouble.nvim", config = function()
+	      require("trouble").setup {
+		  icons = false, -- your configuration comes here
+		  -- or leave it empty to use the default settings
+		  -- refer to the configuration section below
+	      }
+	  end
+      })
+
+      use {
+		'nvim-treesitter/nvim-treesitter', run = function()
+		    local ts_update =
+		    require('nvim-treesitter.install').update({
+		    with_sync = true }) ts_update()
+		end,}
+      use("nvim-treesitter/playground") use("theprimeagen/harpoon")
+      use("theprimeagen/refactoring.nvim") use("mbbill/undotree")
+      use("tpope/vim-fugitive")
+      use("nvim-treesitter/nvim-treesitter-context");
+end)
+
